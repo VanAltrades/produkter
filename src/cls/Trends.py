643dbef6d31cccde3a11887e8c3075b236b07824
VanAltrades <vanaltrades@gmail.com>
2023-12-17 17:19:39 -0700

    add workable Trends

--- src/cls/Trends.py
+++ src/cls/Trends.py
@@ -5,10 +5,19 @@ class Trends:
     
     # def __init__(self, language_processor_instance, keyword):
     def __init__(self, keyword):    
-        self.pytrends = TrendReq(hl='en-US')
+        self.pytrends = TrendReq(hl='en-US', 
+                                 tz=360, 
+                                 timeout=(10,25), 
+                                #  proxies=['https://34.203.233.13:80',], 
+                                 retries=2, 
+                                 backoff_factor=0.1, 
+                                 requests_args={'verify':False})
 
         self.keyword = keyword
-        self.payload_related = self.build_keyword_payload()
+        self.keyword_dictionary = self.build_keyword_payload_dictionary()
+        self.keyword_related_dictionary = self.get_keyword_related_keywords()
+        self.keyword_rising_dictionary = self.get_keyword_rising_keywords()
+
         #1. get related keywords to the engine's `keyword`
         # self.related = self.get_keyword_related_keywords()      
         
@@ -18,7 +27,7 @@ class Trends:
         # if not isinstance(self.keyword_related_list, list):
         #     raise TypeError("The 'keyword_related_list' from `get_related_keywords` must be a list.")
 
-        # self.payload_interest = self.build_interest_payload()
+
         # # TODO: may delete since prioritizing trends' related keywords instead of nlp items
         # self.keyword_items_list_ = [item for item in language_processor_instance.items if isinstance(item, str) and len(item) < 100]
         # if not isinstance(self.keyword_list, list):
@@ -26,14 +35,17 @@ class Trends:
 
 
         # 3. 
-        # self.weekly = self.get_interest_trends()
+        self.payload_interest = self.build_interest_payload()
+        self.weekly = self.get_interest_trends()
         # self.rising = self.get_rising_keywords()
 
 
-    def build_keyword_payload(self):
+    def build_keyword_payload_dictionary(self):
         try:
-            self.pytrends.build_payload(self.keyword)
-            return self.pytrends.related_queries()
+            self.pytrends.build_payload([self.keyword], cat=0, timeframe='today 5-y', geo='US')
+            payload = self.pytrends.related_queries()
+            keyword_dictionary = payload.get(self.keyword)
+            return keyword_dictionary
         except Exception as e:
             print(e)
             return None
@@ -41,9 +53,16 @@ class Trends:
 
     def get_keyword_related_keywords(self):
         top_related_dict = {}
-        top_related = self.payload_related.get(self.keyword)['top']  # DataFrame of top related queries
+        top_related = self.keyword_dictionary['top']  # DataFrame of top related queries
         top_related_dict[self.keyword] = top_related.to_dict(orient='records')
         return top_related_dict
+
+
+    def get_keyword_rising_keywords(self):
+        top_rising_dict = {}
+        top_rising = self.keyword_dictionary['rising']  # DataFrame of top related queries
+        top_rising_dict[self.keyword] = top_rising.to_dict(orient='records')
+        return top_rising_dict
 
 
     def build_related_keywords_payload(self):
@@ -54,26 +73,26 @@ class Trends:
             return None
         
     
-    def get_related_keywords_related_keywords(self):
-        top_related_dict = {}
+    # def get_related_keywords_related_keywords(self):
+    #     top_related_dict = {}
 
-        # case when using Engine's keyword because no related keywords yet
-        if self.keyword_related_list[:3] is None: 
-            # for keyword in self.keyword_list:
-            top_related = self.payload_related.get(self.keyword)['top']  # DataFrame of top related queries
-            top_related_dict[self.keyword] = top_related.to_dict(orient='records')
-            return top_related_dict
-        # case when related keywords already collected so loop through top ones
-        else:
-            for keyword in self.keyword_related_list:
-                top_related = self.payload_related.get(keyword)['top']  # DataFrame of top related queries
-                top_related_dict[keyword] = top_related.to_dict(orient='records')
-                return top_related_dict
+    #     # case when using Engine's keyword because no related keywords yet
+    #     if self.keyword_related_list[:3] is None: 
+    #         # for keyword in self.keyword_list:
+    #         top_related = self.payload_related.get(self.keyword)['top']  # DataFrame of top related queries
+    #         top_related_dict[self.keyword] = top_related.to_dict(orient='records')
+    #         return top_related_dict
+    #     # case when related keywords already collected so loop through top ones
+    #     else:
+    #         for keyword in self.keyword_related_list:
+    #             top_related = self.payload_related.get(keyword)['top']  # DataFrame of top related queries
+    #             top_related_dict[keyword] = top_related.to_dict(orient='records')
+    #             return top_related_dict
     
 
     def build_interest_payload(self):
         try:
-            self.pytrends.build_payload(self.keyword_list, cat=0, timeframe='today 5-y', geo='US')
+            self.pytrends.build_payload([self.keyword], cat=0, timeframe='today 5-y', geo='US')
             return self.pytrends.interest_over_time()
         except Exception as e:
             print(e)
@@ -83,13 +102,5 @@ class Trends:
     def get_interest_trends(self):
         df = self.payload_interest.drop(
             self.payload_interest[self.payload_interest['isPartial'] == True].index)
-        df = df[self.keyword_list]
+        # df = df[self.keyword]
         return df.to_json(orient='index', date_format='iso')
-    
-
-    def get_rising_keywords(self):
-        rising_related_dict = {}
-        for keyword in self.keyword_list:
-            rising_related = self.payload_related.get(keyword)['rising']  # DataFrame of rising related queries
-            rising_related_dict[keyword] = rising_related.to_dict(orient='records')
-        return rising_related_dict
